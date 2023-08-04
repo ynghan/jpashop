9f5eb144ba96a957b6a3caeb300d121545fc267b
ynghan <wjddudgks5669@gmail.com>
2023-08-05 01:57:42 +0900

    query optimization complete

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -92,10 +92,24 @@ public class OrderRepository {
         ).getResultList();
     }
 
-    public List<OrderSimpleQueryDto> findOrderDtos() {
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
         return em.createQuery(
                 "select o from Order o" +
-                        "join o.member m" +
-                        "join o.delivery d",OrderSimpleQueryDto.class).getResultList();
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
     }
 }
+
--- src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryDto.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop.repository;
+package jpabook.jpashop.repository.order.simplequery;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
@@ -15,11 +15,11 @@ public class OrderSimpleQueryDto {
     private OrderStatus orderStatus;
     private Address address;
 
-    public OrderSimpleQueryDto(Order order) {
-        orderId = order.getId();
-        name = order.getMember().getName();
-        orderDate = order.getOrderDate();
-        orderStatus = order.getStatus();
-        address = order.getDelivery().getAddress();
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
     }
 }
